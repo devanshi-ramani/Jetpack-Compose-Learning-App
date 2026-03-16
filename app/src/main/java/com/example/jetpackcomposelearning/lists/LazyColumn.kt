dd1b1f0c769b95f680c6ca6ed716eefe11d8dd2c
Devanshi Ramani <devanshiramani885@gmail.com>
2026-03-16 15:10:20 +0530

    making lazycolumn and lazyrow

--- app/src/main/java/com/example/jetpackcomposelearning/lists/LazyColumn.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/lists/LazyColumn.kt
@@ -1,2 +1,81 @@
 package com.example.jetpackcomposelearning.lists
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposelearning.ui.theme.Pink80
+import com.example.jetpackcomposelearning.ui.theme.PurpleGrey40
+import com.example.jetpackcomposelearning.ui_components.DemoCard
+
+@Composable
+fun lazycolumnComponent(onBackClick:() -> Unit) {
+    Screen(
+        title = "LazyColumn",
+        onBackClick = onBackClick
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 30.dp, horizontal = 30.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(containerColor = Pink80)
+                ) {
+                    Text("Back")
+                }
+            }
+        }
+
+        DemoCard(
+            "Lazy Row Example",
+            "Below contains item and items from lazy row which is horizontal."
+        ) {
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                item {
+                    Text(text = "First item")
+                }
+
+                items(5) { index ->
+                    Text(text = "Item : $index")
+                }
+
+                item {
+                    Text(text = "Last item")
+                }
+
+
+            }
+        }
+    }
+}
